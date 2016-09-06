54381a33f27436fef0bdda82f781374118c97a77
saurabhjagdhane <saurabhjagdhane@gmail.com>
2016-09-05 17:30:49 -0700

    Volley module included....Network and cache setup remaining.

--- app/src/main/java/com/example/saurabh/auggraffiti/CameraActivity.java
+++ app/src/main/java/com/example/saurabh/auggraffiti/CameraActivity.java
@@ -1,13 +1,58 @@
 package com.example.saurabh.auggraffiti;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class CameraActivity extends AppCompatActivity {
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Button capture;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        capture = (Button)findViewById(R.id.capture_button);
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CameraActivity.this,DrawingScreen.class );
+                startActivity(i);
+            }
+        });
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
---
+++ app/src/main/java/com/example/saurabh/auggraffiti/DrawingScreen.java
@@ -0,0 +1,18 @@
+package com.example.saurabh.auggraffiti;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DrawingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new TouchEventView(DrawingScreen.this,null));
+    }
+
+}
